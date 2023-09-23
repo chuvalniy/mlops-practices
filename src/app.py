b54e28b1d1dff30f6d9d831cb27cb3512549d759
Chuvalniy <chuvalik@gmail.com>
2023-09-23 14:41:07 +0300

    added post request to flask app to access ai model

--- src/app.py
+++ src/app.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 
 from models.model import generate_and_decode_text
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.post("/generate")
 def generate():
-    text = generate_and_decode_text(500)
+    data = request.json
+    try:
+        sample = data['text_length']
+    except KeyError:
+        return jsonify({"error": "No text length sent. You have to use 'text_length' as key to your request."})
+
+    text = generate_and_decode_text(sample)
 
     return text
 
